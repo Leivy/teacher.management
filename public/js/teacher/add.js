3c03746982650082d5d79171e6262e73a56dab83
Leivy <1339765939@qq.com>
2017-08-21 19:34:18 +0800

    教师列表添加编辑功能初步完成待完善

--- public/js/teacher/add.js
+++ public/js/teacher/add.js
@@ -1,12 +1,46 @@
-define(['jquery'], function ($) {
-  $.ajax({
-    type: 'get',
-    url: '/api/teacher/edit',
-    data: {
-      tc_id: 2
-    },
-    success: function (info) {
-      console.log(info);
+define(['jquery', 'template', 'jquery_cookie'], function ($, template) {
+  $(function () {
+    //list.html页面点击编辑时已经存储下tc_id到cookie中
+    var tc_id = $.cookie('tc_id');
+    if ($.cookie('tc_id')) {
+      $.ajax({
+        type: 'get',
+        url: '/api/teacher/edit',
+        data: {
+          tc_id: tc_id
+        },
+        success: function (info) {
+          if (info.code == 200) {
+            var html = template('teacheraddtemp', info.result);
+            $(".teacher-add").html(html);
+          }
+        }
+      });
     }
+
+    //添加按钮功能 点击事件发送请求修改教师信息 委托事件
+    $('.teacher-add').on('click', '#editor_add', function () {
+      $that = $(this);
+      $.ajax({
+        type: 'post',
+        url: '/api/teacher/update',
+        data: {
+          tc_id: tc_id,
+          tc_name: $("#tc_name").val(),
+          tc_join_date: $("#tc_join_date").val(),
+          tc_type: $('.tc_type>option:selected').text() == '讲师' ? 1 : 0,
+          tc_gender: $('.tc_gender input:checked').text() == '男' ? 0 : 1,
+        },
+        success: function (info) {
+          if (info.code == 200) {
+            alert('save success');
+            // location.href = $that.attr('href');
+          }
+        }
+      });
+
+      return false;
+    })
+
   })
 })
--- public/js/teacher/list.js
+++ public/js/teacher/list.js
@@ -1,69 +1,83 @@
 define([
   'jquery',
   'template',
-  'bootstrap'
+  'bootstrap',
+  'jquery_cookie'
 ], function ($, template) {
   'use strict';
-  //拉取数据渲染列表
-  $.ajax({
-    type: "get",
-    url: '/api/teacher',
-    success: function (info) {
-      if (info.code == 200) {
-        var html = template("teacher_list_temp", info);
-        $("#teacherlist").html(html);
-      }
-    }
-  });
-  //查看按钮功能实现 点击事件 显示模态框 更新模态框数据
-  //由于列表信息是ajax渲染的 所以注册委托事件
-  $("#teacherlist").on('click', "#check", function () {
-    console.log(123)
-    $("#teacherModal").modal(); //显示模态框
-    //发送ajax请求 渲染模态框的内容
-    var tc_id = $(this).parent().data("id");
+  $(function () {
+    //拉取数据渲染列表
     $.ajax({
-      type: 'get',
-      url: '/api/teacher/view',
-      data: {
-        tc_id: tc_id
-      },
+      type: "get",
+      url: '/api/teacher',
       success: function (info) {
         if (info.code == 200) {
-          var html = template("moduletemp", info.result);
-          $("#teacherModal").html(html);
+          var html = template("teacher_list_temp", info);
+          $("#teacherlist").html(html);
         }
       }
-    })
-    return false; //点击事件都要阻止默认行为 阻止跳转
-  });
-
-  //注销按钮功能实现 点击事件  发送请求注销 后台就会注销并更新数据
-
-  $("#teacherlist").on('click', "#delete", function () {
-    var tc_id = $(this).parent().data("id");
-    var tc_status = $(this).parent().data("status");
-    var $that = $(this);
-    $.ajax({
-      type: 'post',
-      url: '/api/teacher/handle',
-      data: {
-        tc_id: tc_id,
-        tc_status: tc_status
-      },
-      success: function (info) {
-        console.log(info.result.tc_status);
-        if (info.code == 200) {
-          if (info.result.tc_status == 1) {
-            $that.html('启用').removeClass('btn-warning').addClass('btn-success');
-          } else {
-            $that.html('注销').removeClass('btn-success').addClass('btn-warning');
+    });
+    //查看按钮功能实现 点击事件 显示模态框 更新模态框数据
+    //由于列表信息是ajax渲染的 所以注册委托事件
+    $("#teacherlist").on('click', "#check", function () {
+      console.log(123)
+      $("#teacherModal").modal(); //显示模态框
+      //发送ajax请求 渲染模态框的内容
+      var tc_id = $(this).parent().data("id");
+      $.ajax({
+        type: 'get',
+        url: '/api/teacher/view',
+        data: {
+          tc_id: tc_id
+        },
+        success: function (info) {
+          if (info.code == 200) {
+            var html = template("moduletemp", info.result);
+            $("#teacherModal").html(html);
           }
-          $that.parent().data('status', info.result.tc_status);
         }
-      }
-    })
-  });
+      })
+      return false; //点击事件都要阻止默认行为 阻止跳转
+    });
 
-  //编辑按钮功能实现  
+    //注销按钮功能实现 点击事件  发送请求注销 后台就会注销并更新数据
+
+    $("#teacherlist").on('click', "#delete", function () {
+      var tc_id = $(this).parent().data("id");
+      var tc_status = $(this).parent().data("status");
+      var $that = $(this);
+      $.ajax({
+        type: 'post',
+        url: '/api/teacher/handle',
+        data: {
+          tc_id: tc_id,
+          tc_status: tc_status
+        },
+        success: function (info) {
+          console.log(info.result.tc_status);
+          if (info.code == 200) {
+            if (info.result.tc_status == 1) {
+              $that.html('启用').removeClass('btn-warning').addClass('btn-success');
+            } else {
+              $that.html('注销').removeClass('btn-success').addClass('btn-warning');
+            }
+            $that.parent().data('status', info.result.tc_status);
+          }
+        }
+      })
+    });
+
+    //编辑按钮功能实现  存储tc_id到cookie中给到add.html
+    $("#teacherlist").on('click', '#editor', function () {
+      $.cookie('tc_id', $(this).parent().data('id'));
+    });
+
+    //添加教师按钮功能 跳转到add.html 清空cookie中的tc_id
+    $("#addnewteacher").on('click', function () {
+      $.removeCookie('tc_id');
+      console.log($(this).attr('href'))
+      location.href = $(this).attr('href');
+      return false;
+    })
+  })
 });
